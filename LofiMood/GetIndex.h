a0845a41cfeb85e2e4f83110718a191ed1656424
Ryan James <realcoder2023@gmail.com>
2025-06-15 20:43:21 -0400

    added custom types

---
+++ LofiMood/GetIndex.h
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace Index {
+
+	static std::string IndexPoint(std::string a) {
+		
+		return a;
+	}
+}
--- LofiMood/main.cpp
+++ LofiMood/main.cpp
@@ -1,5 +1,6 @@
 #include "Fstream.cpp"
 #include "Imports.h"
+#include "GetIndex.h";
 
 namespace PointPoint {
 
@@ -14,22 +15,107 @@ namespace PointPoint {
 
 }
 
+enum Option {
+
+	YOUTUBE,
+	GOOGLE,
+	AMAZON,
+	GMAIL,
+	TWITTER,
+	TICTOK
+
+};
+
+
 int main() {
+
+
+	const char* types[] = { "YOUTUBE", "GOOGLE", "AMAZON", "GMAIL", "TWITTER", "TICTOK" };
+	std::string YouTube = types[YOUTUBE];
+	std::string Google = types[GOOGLE];
+	std::string Amazon = types[AMAZON];
+	std::string Gmail = types[GMAIL];
+	std::string Twitter = types[TWITTER];
+	std::string TicTok = types[TICTOK];
+
 
 	std::string OPTION;
 	std::string URL;
 
+	std::string INDEX;
 	std::cout << "Enter URL: " << "\n";
 
 	std::cin >> URL;
+
+	if (URL == "YT") {
+
+		std::cout << YouTube << std::endl;
+
+	}
+
+	else if (URL == "GOOGLE") {
+
+		std::cout << Google << std::endl;
+
+	}
+
+	else if (URL == "Amazon") {
+
+		std::cout << Amazon << std::endl;
+
+	}
+
+
+	else if (URL == "Gmail") {
+
+		std::cout << Gmail << std::endl;
+
+	}
+
+
+	else if (URL == "Twitter") {
+
+		std::cout << Twitter << std::endl;
+
+	}
+
+
+	else if (URL == "TikTok") {
+
+		std::cout << Twitter << std::endl;
+
+	}
+
+
 
 	ShellExecuteA(nullptr, "open", URL.c_str(), nullptr, nullptr, SW_SHOWDEFAULT);
 
 	std::cout << "Save to File Y | N: ";
 	std::cin >> OPTION;
 
+	std::cout << "Enter Index: " << "\n";
+	std::cin >> INDEX;
+
+
+
+	Index::IndexPoint(INDEX.c_str());
+
+	if (INDEX.c_str() == "1") {
+
+		if (OPTION == "Y") {
+
+			std::cout << "URL: " << *PointPoint::printPointer(URL);
+
+			LofiMoodFile(URL);
+		}
+
+
+	}
+
 	if (OPTION == "Y") {
 	
+
+
 		std::cout << "URL: " << *PointPoint::printPointer(URL);
 
 		LofiMoodFile(URL);
